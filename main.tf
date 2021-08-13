604ca98d4d331f7aeb02311f235a43ec73d8ddd5
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-13 11:00:28 +0200

    added variables and outputs

--- main.tf
+++ main.tf
@@ -9,18 +9,12 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "eu-north-1"
-  access_key = "AKIATYESCEMZQUUCPP77"
-  secret_key = "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"
-}
-
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "app-server"
+  access_key = "AKIATYESCEMZZVGIQCUO"
+  secret_key = "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"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0567085e558e02053"
+  ami           = "ami-00888f2a5f9be4390"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-d8f15ebe"]
   subnet_id              = "subnet-91e73adc"
